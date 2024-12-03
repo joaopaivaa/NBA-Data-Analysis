f820f60aae8acc8c8432a5015d582a7b243b275f
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2024-12-03 20:52:43 -0300

    Chart modifications

--- nba_data_analysis.py
+++ nba_data_analysis.py
@@ -19,7 +19,7 @@ if player_id:
 
     data['GAME_DATE'] = pd.to_datetime(data['GAME_DATE'])
 
-    window_size = 5
+    window_size = 10
     for column in data.columns[6:-2]:
         data[f'{column}_Moving_Avg'] = data[column].rolling(window=window_size).mean()
     data['PLUS_MINUS_Moving_Sum'] = data['PLUS_MINUS'].rolling(window=window_size).sum()
@@ -33,6 +33,9 @@ if player_id:
     for i in range(len(columns)):
         last_avg = round(data[f'{columns[i]}_Moving_Avg'].iloc[-1],2)
         mean = round(data[columns[i]].mean(),2)
+        perc_above = round(100*len(data[data[columns[i]] > mean]) / len(data), 2)
+        perc_under = round(100*len(data[data[columns[i]] < mean]) / len(data), 2)
+        perc_diff = round(100*(last_avg-mean)/mean, 2)
         ax[row,column].set_title(columns[i])
         ax[row,column].set_ylim(0, max(data[columns[i]])*1.1)
         ax[row,column].tick_params(axis='x', size=6)
@@ -41,16 +44,18 @@ if player_id:
         ax[row,column].xaxis.set_major_formatter(mdates.DateFormatter('%b'))
         ax[row,column].scatter(data['GAME_DATE'], data[columns[i]], color='gray', alpha=0.5, label=columns[i])
         #ax[row,column].scatter(data['GAME_DATE'], data[columns[i]])
-        ax[row,column].plot(data['GAME_DATE'], data[f'{columns[i]}_Moving_Avg'], color='blue', label=f'Moving Average: {last_avg}')
+        ax[row,column].plot(data['GAME_DATE'], data[f'{columns[i]}_Moving_Avg'], color='blue', label=f'Moving Average: {last_avg} ({perc_diff}%)')
         #ax[row,column].scatter(data['GAME_DATE'], data[f'{columns[i]}_Moving_Avg'])
         ax[row,column].axhline(mean, color='red', alpha=0.5, ls='--', label=f'Mean: {mean}')
-        ax[row,column].legend()
+        ax[row,column].plot([], [], label=f'Above mean: {perc_above}%')
+        ax[row,column].plot([], [], label=f'Under mean: {perc_under}%')
+        ax[row,column].legend(fontsize=6)
         if i % 2 == 0:
             row = 1
         else:
             row = 0
             column += 1
-    plt.suptitle('Last 5 Games Moving Average')
+    plt.suptitle(f'Last 5 Games Moving Average: {player_name}')
     plt.tight_layout()
     plt.show()
 
