e641fd322c97674b7323e78b00d30898fc39736b
João Vitor de Paiva Marcotti <joaovmarcotti@hotmail.com>
2024-12-04 22:02:40 -0300

    Add 2 functions

--- all_players_data.py
+++ all_players_data.py
@@ -3,4 +3,9 @@ from nba_api.stats.endpoints import leaguedashplayerstats
 season = "2024-25"
 
 players_stats = leaguedashplayerstats.LeagueDashPlayerStats(season=season)
-all_players_df = players_stats.get_data_frames()[0]
+players_stats_df = players_stats.get_data_frames()[0]
+
+players_advanced_stats = leaguedashplayerstats.LeagueDashPlayerStats(season=season,
+                                                                     measure_type_detailed_defense='Advanced',  # Estatísticas avançadas
+                                                                     per_mode_detailed='PerGame')
+players_advanced_stats_df = players_advanced_stats.get_data_frames()[0]
